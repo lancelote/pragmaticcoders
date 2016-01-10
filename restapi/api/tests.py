76f44c2431f75870ba882dd41626a8f3bc7546db
Pavel Karateev <karateev.pavel@ya.ru>
2016-01-10 16:17:37 +0300

    Adds tests for get_10_by_person and get_10_by_time

--- restapi/api/tests.py
+++ restapi/api/tests.py
@@ -2,7 +2,7 @@ import unittest
 
 from .views import (
     parse_event, add_event_to_storage,
-    get_10_by_category
+    get_10_by_category, get_10_by_person, get_10_by_time
 )
 from .models import data
 
@@ -81,3 +81,74 @@ class TestGet10ByCategory(unittest.TestCase):
         self.assertTrue(check_the_storage(last_event, last_10_by_category))
         self.assertTrue(check_the_storage(sample_event, last_10_by_category))
 
+
+class TestGet10ByPerson(unittest.TestCase):
+
+    def setUp(self):
+        events = [
+            'text1 #category1 @person1',
+            'text2 #category2 @person1',
+            'text3 #category3 @person1',
+            'text4 #category4 @person1',
+            'text5 #category5 @person1',
+            'text6 #category6 @person2',
+            'text7 #category7 @person1',
+            'text8 #category8 @person1',
+            'text9 #category9 @person1',
+            'text10 #category10 @person1',
+            'text11 #category11 @person1',
+            'text12 #category12 @person1',
+        ]
+        for event in events:
+            add_event_to_storage(parse_event(event))
+
+    def test_return_correct_result(self):
+        last_10_by_person = get_10_by_person('person1')
+        self.assertEqual(len(last_10_by_person), 10)
+
+        to_old = parse_event('text1 #category1 @person1')
+        wrong_person = parse_event('text6 #category6 @person2')
+        last_event = parse_event('text12 #category12 @person1')
+        sample_event = parse_event('text7 #category7 @person1')
+
+        self.assertFalse(check_the_storage(to_old, last_10_by_person))
+        self.assertFalse(check_the_storage(wrong_person, last_10_by_person))
+        self.assertTrue(check_the_storage(last_event, last_10_by_person))
+        self.assertTrue(check_the_storage(sample_event, last_10_by_person))
+
+
+class TestGet10ByTime(unittest.TestCase):
+
+    def setUp(self):
+        events = [
+            'text1 #category1 @person1',
+            'text2 #category2 @person2',
+            'text3 #category3 @person3',
+            'text4 #category4 @person4',
+            'text5 #category5 @person5',
+            'text6 #category6 @person6',
+            'text7 #category7 @person7',
+            'text8 #category8 @person8',
+            'text9 #category9 @person9',
+            'text10 #category10 @person10',
+            'text11 #category11 @person11',
+            'text12 #category12 @person12',
+        ]
+        for event in events:
+            add_event_to_storage(parse_event(event))
+
+    def test_returns_correct_result(self):
+        last_10_by_time = get_10_by_time()
+        self.assertEqual(len(last_10_by_time), 10)
+
+        to_old1 = parse_event('text1 #category1 @person1')
+        to_old2 = parse_event('text2 #category2 @person2')
+        first = parse_event('text3 #category3 @person3')
+        sample = parse_event('text8 #category8 @person8')
+        last = parse_event('text12 #category12 @person12')
+
+        self.assertFalse(check_the_storage(to_old1, last_10_by_time))
+        self.assertFalse(check_the_storage(to_old2, last_10_by_time))
+        self.assertTrue(check_the_storage(first, last_10_by_time))
+        self.assertTrue(check_the_storage(sample, last_10_by_time))
+        self.assertTrue(check_the_storage(last, last_10_by_time))
